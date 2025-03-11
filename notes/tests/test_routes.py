6b37f1c2c812537f000a15d5e7ebeaf1b0f18455
Relistic <s.nedviga@relistic.ru>
2025-03-11 20:54:20 +0300

    test_routes исправлены

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -1,15 +1,23 @@
 from http import HTTPStatus
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse_lazy
-
 from notes.models import Note
 
 User = get_user_model()
 
 
 class TestRoutes(TestCase):
+    # Фикстуры для маршрутов
+    home_url = 'notes:home'
+    signup_url = 'users:signup'
+    login_url = 'users:login'
+    list_url = 'notes:list'
+    add_url = 'notes:add'
+    success_url = 'notes:success'
+    edit_url = 'notes:edit'
+    delete_url = 'notes:delete'
+    detail_url = 'notes:detail'
 
     @classmethod
     def setUpTestData(cls):
@@ -19,14 +27,15 @@ class TestRoutes(TestCase):
             title='Название заметки',
             text='Текст заметки',
             slug='note',
-            author=cls.author)
+            author=cls.author
+        )
 
     def test_pages_availability(self):
         """Страницы(index, регистрация, вход и выход) доступны всем"""
         urls = (
-            'notes:home',
-            'users:signup',
-            'users:login'
+            self.home_url,
+            self.signup_url,
+            self.login_url,
             # 'users:logout',
         )
         for name in urls:
@@ -37,18 +46,18 @@ class TestRoutes(TestCase):
     def test_redirect_user(self):
         """Редирект анонимных пользователей"""
         urls = (
-            ('notes:list', None),
-            ('notes:add', None),
-            ('notes:edit', self.note.slug,),
-            ('notes:delete', self.note.slug,),
-            ('notes:detail', self.note.slug,)
+            (self.list_url, []),
+            (self.add_url, []),
+            (self.edit_url, [self.note.slug]),
+            (self.delete_url, [self.note.slug]),
+            (self.detail_url, [self.note.slug]),
         )
         for name, args in urls:
             with self.subTest(name=name):
-                url = reverse_lazy(name, args=[args] if args else [])
+                url = reverse_lazy(name, args=args)
                 response = self.client.get(url)
                 self.assertRedirects(
-                    response, f"{reverse_lazy('users:login')}?next={url}")
+                    response, f"{reverse_lazy(self.login_url)}?next={url}")
 
     def test_authenticated_user_access(self):
         """Аутентифицированному пользователю доступны
@@ -56,9 +65,9 @@ class TestRoutes(TestCase):
         """
         self.client.force_login(self.author)
         urls = (
-            'notes:list',
-            'notes:add',
-            'notes:success',
+            self.list_url,
+            self.add_url,
+            self.success_url,
         )
         for name in urls:
             with self.subTest(name=name):
@@ -74,9 +83,9 @@ class TestRoutes(TestCase):
             (self.not_author, HTTPStatus.NOT_FOUND),
         )
         urls = (
-            ('notes:edit', self.note.slug),
-            ('notes:delete', self.note.slug),
-            ('notes:detail', self.note.slug),
+            (self.edit_url, self.note.slug),
+            (self.delete_url, self.note.slug),
+            (self.detail_url, self.note.slug),
         )
         for user, expected_status in users_statuses:
             with self.subTest(user=user.username):
